abf467993c8d6c4505ba325d3a0c6faa30e8983d
rogerskev17 <rogerskev17@gmail.com>
2025-05-02 15:36:33 -0500

    Accessed pizza table data

--- Pages/Menu.cshtml.cs
+++ Pages/Menu.cshtml.cs
@@ -1,12 +1,24 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RogersPizza.Data;
+using RogersPizza.Models;
 
 namespace RogersPizza.Pages
 {
     public class MenuModel : PageModel
     {
-        public void OnGet()
+        private readonly RogersPizza.Data.StoreContext _context;
+
+        public MenuModel(RogersPizza.Data.StoreContext context)
         {
-            
+            _context = context;
+        }
+
+        public IList<Pizza> Pizzas { get; set; }
+        public async Task OnGetAsync()
+        {
+            Pizzas = await _context.Pizzas.ToListAsync();
         }
     }
 }
