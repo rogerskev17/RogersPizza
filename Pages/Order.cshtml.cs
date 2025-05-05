9d400cbc91a79874d9495c900e40279ebe45d0d0
rogerskev17 <rogerskev17@gmail.com>
2025-05-05 15:38:19 -0500

    Displayed pizza menu, started on order page, minor refactoring

---
+++ Pages/Order.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RogersPizza.Models;
+
+namespace RogersPizza.Pages
+{
+    public class OrderModel : PageModel
+    {
+        private readonly RogersPizza.Data.StoreContext _context;
+
+        public OrderModel(RogersPizza.Data.StoreContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Pizza> Pizzas { get; set; }
+        public async Task OnGetAsync()
+        {
+            Pizzas = await _context.Pizzas.ToListAsync();
+        }
+
+    }
+}
